1887ebb247979e1a5d291c36a7a430569882e417
Guillaume Briday <guillaumebriday@gmail.com>
2016-05-13 22:07:32 +0200

    Ajout de users dans les seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  username: 'Grimmer',
+  email: "grimmer@gmail.com",
+  first_name: 'Grimmer',
+  last_name: "Grimmer",
+  password: "password",
+  auth_token: "6031813070"
+)
+
+User.create(
+  username: 'Ash',
+  email: "ash@gmail.com",
+  first_name: 'Ash',
+  last_name: "Ash",
+  password: "password",
+  auth_token: "8154113283"
+)
+
+User.create(
+  username: 'Timmer',
+  email: "timmer@gmail.com",
+  first_name: 'Timmer',
+  last_name: "Timmer",
+  password: "password",
+  auth_token: "3167332618"
+)
