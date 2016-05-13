1e91f70688bb225a5634e81bc617303f30e6314a
Guillaume Briday <guillaumebriday@gmail.com>
2016-05-14 00:29:50 +0200

    Modification de la configuration de la precompalition des assets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module HotelTycoon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.enabled = false
+    config.assets.initialize_on_precompile = false
   end
 end
