4db5158870383c6be8259ef572e8ee1b2f9b6a43
Guillaume Briday <guillaumebriday@gmail.com>
2016-05-13 22:08:04 +0200

    Ajout du model User

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  before_create :set_auth_token
+  has_secure_password
+
+  validates_presence_of :first_name, :last_name
+  validates :password,
+            presence: true,
+            length: {minimum: 8}
+  validates :username,
+            presence: true,
+            uniqueness: true
+
+  def self.activated
+    where(is_activated: 1)
+  end
+
+private
+  def set_auth_token
+    return if auth_token.present?
+    self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+    loop do
+      token = SecureRandom.hex
+      break token unless self.class.exists?(auth_token: token)
+    end
+  end
+end
