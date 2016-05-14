acaac44dc961bbf1cf4a35028a3f1d6be2e71c48
Guillaume Briday <guillaumebriday@gmail.com>
2016-05-14 16:26:28 +0200

    Ajout d'une configuration pour mailtrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '54699d9a09c16cb34',
+    :password => '200f56a97aaff4',
+    :address => 'mailtrap.io',
+    :domain => 'mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 end
