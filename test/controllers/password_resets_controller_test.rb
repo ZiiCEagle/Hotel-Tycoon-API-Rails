514921e5fec24d31fd6051f53a5f05624e605829
Guillaume Briday <guillaumebriday@gmail.com>
2016-05-17 16:45:11 +0200

    Correction des tests du password_resets

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @tim = users(:tim)
+  end
+
   test "should get create" do
-    get password_resets_create_url
+    post password_resets_url, {email: @tim.email}
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
     assert_response :success
+    assert_equal Mime[:json], response.content_type
+    assert_equal "Email envoyé", body[:notice]
   end
 
   test "should get edit" do
-    get password_resets_edit_url
+    get edit_password_reset_url(@tim.reset_password_token)
     assert_response :success
   end
 
   test "should get update" do
-    get password_resets_update_url
-    assert_response :success
+    patch password_reset_url(@tim.reset_password_token), {user: {password: "coucoucou", password_confirmation: "coucoucou"}}
+    assert_redirected_to password_resets_url
   end
-
 end
