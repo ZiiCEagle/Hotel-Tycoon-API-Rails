faa2f21795fd6f76028100ac7bea2bc64a45f447
Guillaume Briday <guillaumebriday@gmail.com>
2016-05-17 14:41:15 +0200

    Ajout des méthodes au controller Roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,39 @@
 class RolesController < ApplicationController
   before_action :set_role, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @roles = Role.all
+
+    render json: @roles
+  end
+
+  def show
+    render json: @role
+  end
+
+
+  def create
+    @role = Role.new(role_params)
+
+    if @role.save
+      render json: @role, status: :created, location: @role
+    else
+      render json: @role.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @role.update(role_params)
+      render json: @role
+    else
+      render json: @role.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @role.destroy
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_role
