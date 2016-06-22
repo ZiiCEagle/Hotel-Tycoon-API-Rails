876ba15bb978557d92f44e476cd2069602c7cda1
Guillaume Briday <guillaumebriday@gmail.com>
2016-06-22 10:56:44 +0200

    Correction des exceptions dans les routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   patch 'users/:id/attach_role', to: 'users#attach_role'
   patch 'users/:id/detach_role', to: 'users#detach_role'
   resources :bedroom_types, except: [:edit, :new]
-  resources :roles
-  resources :password_resets
-  resources :users do
-    resources :roles
+  resources :roles, except: [:edit, :new]
+  resources :password_resets, except: [:destroy, :show]
+  resources :users, except: [:edit, :new] do
+    resources :roles, except: [:edit, :new]
   end
   post '/login', to: 'users#login'
 end
