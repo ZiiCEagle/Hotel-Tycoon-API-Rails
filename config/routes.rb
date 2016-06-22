221c97ccd4f86b77ffbcb66cdf7ae2f2c99c3e31
Guillaume Briday <guillaumebriday@gmail.com>
2016-06-22 16:19:11 +0200

    Ajout des chambres aux routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   patch 'users/:id/attach_role', to: 'users#attach_role'
   patch 'users/:id/detach_role', to: 'users#detach_role'
+  resources :bedrooms, except: [:edit, :new]
   resources :bedroom_types, except: [:edit, :new]
   resources :roles, except: [:edit, :new]
   resources :password_resets, except: [:destroy, :show]
